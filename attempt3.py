c51eed4cb990262fd2469eff37bd25eafa54b1a1
bbaous <bbaous@gmail.com>
2022-11-11 08:54:37 +0000

    More comments

--- attempt3.py
+++ attempt3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Python program to convert .tsv file to .csv file
+
+
 # Importing necessary libraries
-import csv
-import numpy as np
 import pandas as pd
 import re
 import argparse
@@ -63,7 +63,7 @@ def writeToFile(InputFile, OutputFile):
 input_file1 = open(readInputArgument().file1, 'r')
 input_file2 = open(readInputArgument().file2, 'r')
 
-# Trying to open csv files and if do not exist create them.
+# Trying to open csv files and if they do not exist create them.
 try:
     output_file1 = open(readInputArgument().output1, 'w')
     print("File Exists")
@@ -82,7 +82,7 @@ except IOError:
 def addColumns(FirstColumn, SecondColumn):
     return FirstColumn+SecondColumn
 
-# Divide two df -replacing 0 with one to remove NaN
+# Divide two df -replacing 0 on the second df with 1 to remove NaN
 def findMean(FirstColumn, SecondColumn):
     SecondColumn.replace(to_replace=0, value=1,  inplace=True)
     return FirstColumn/SecondColumn
@@ -92,16 +92,18 @@ def findMean(FirstColumn, SecondColumn):
 writeToFile(InputFile=input_file1, OutputFile=output_file1)
 writeToFile(InputFile=input_file2, OutputFile=output_file2)
 
+#Load the output files
 csvfile1 = pd.read_csv(readInputArgument().output1)
 csvfile2 = pd.read_csv(readInputArgument().output2)
-# add column 3 in csv 1 to column 3 in second csv (add ref_allele_counts)
-result = addColumns(csvfile1['Ref_Allele_Count'], csvfile2['Ref_Allele_Count'])
 
-# add column 4 in csv 1 to column 4 in second csv (add alt_allele_cunts)
+# Add column 3 in csv 1 to column 3 in second csv (add ref_allele_counts)
+result1 = addColumns(csvfile1['Ref_Allele_Count'], csvfile2['Ref_Allele_Count'])
+
+# Add column 4 in csv 1 to column 4 in second csv (add alt_allele_cunts)
 result2 = addColumns(csvfile1['Alt_Allele_Count'],
                      csvfile2['Alt_Allele_Count'])
 
-# add column 5 in csv 1 to column 5 in second csv (add cover depth)
+# Add column 5 in csv 1 to column 5 in second csv (add cover depth)
 result3 = addColumns(csvfile1['Coverage_Depth'], csvfile2['Coverage_Depth'])
 
 # Find the mean between alt_allele frequencies in csv1 and csv2
@@ -109,4 +111,4 @@ result4 = findMean(result2, result3)
 
 # Writing the results to a csv file
 with open('output.csv', 'w') as f:
-    pd.concat([result, result2, result3, result4], axis=1).to_csv(f)
+    pd.concat([result1, result2, result3, result4], axis=1).to_csv(f)
