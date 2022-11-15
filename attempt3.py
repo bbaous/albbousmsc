7ae16c5c3ad58fb091071f8a1b0815bfad9866fc
bbaous <bbaous@gmail.com>
2022-11-15 20:06:27 +0000

    Added result 4 name

--- attempt3.py
+++ attempt3.py
@@ -115,7 +115,7 @@ result3 = addColumns(csvfile1['Coverage_Depth'], csvfile2['Coverage_Depth'])
 
 # Find the mean between alt_allele frequencies in csv1 and csv2
 result4 = findMean(result2, result3)
-#result4.replace(to_replace='', value=0, inplace=True)
+result4.name='Alt_Allele_Frequency'
 
 
 # Writing the results to a csv file
